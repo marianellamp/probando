c98275cfc666b3dbb5a59670ea003c5558722f30
alumnoTarde <alumnoTarde@DESKTOP-FEO59Q3>
2024-03-13 17:06:43 +0100

    segunda modificación

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT!!");
 		System.out.println("modificación primera");
+		System.out.println("modificación segunda");
 	}
 
 }
