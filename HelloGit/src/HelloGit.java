c1a6edc269b2842fe739d7984aa3f8d3c08dc16e
mane <89552967+marianellamp@users.noreply.github.com>
2024-03-14 18:12:17 +0100

    Update HelloGit.java
    comentario 1234

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -2,6 +2,7 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
+		// 1234
 		System.out.println("Hello GIT!!");
 		System.out.println("modificación primera");
 		System.out.println("modificación segunda");
